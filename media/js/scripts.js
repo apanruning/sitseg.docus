5867b3da72223ac236f83868da5a8d19ed86eddd
Matías Iturburu <maturburu@gmail.com>
2011-12-04 16:10:40 -0300

    Refactorizado un poco las vistas

--- media/js/scripts.js
+++ media/js/scripts.js
@@ -1,16 +1,4 @@
 $('document').ready(function(){
-    function GetFilename(url)
-    {
-       if (url)
-       {
-          var m = url.toString().match(/.*\/(.+?)\./);
-          if (m && m.length > 1)
-          {
-             return m[1];
-          }
-       }
-       return "";
-    }
     function column_form_change(){
         form = $(this).parents('form');
         data = $(form).serializeArray();
@@ -53,7 +41,22 @@ $('document').ready(function(){
     window.setTimeout(function(){
         $('#messages .control').click()
     }, 3000);
-    
-      
+    $('.pjax').click(function(){
+        $('#ajax-container').remove();
+        target = $(this).attr('href');
+        container = $(this).parents('li');
+        $.get(
+            target,
+            function(data){
+                wrapper = $('<div">').attr('id','ajax-container').append(data);
+                $(container).append(wrapper).effect('highlight', {}, 2000);
+                $(container).find('#accordion').accordion();
+            }
+        
+        );
+        return false;
+        
+        
+    })
     
 })
