b6269c68bb484a3c8dcf04ff9394e01aca4654da
Matías Iturburu <maturburu@gmail.com>
2011-12-08 03:11:58 -0300

    Avances en el detalle de datoos

--- urls.py
+++ urls.py
@@ -21,7 +21,7 @@ urlpatterns = patterns('datasources.views',
 
     (r'^datasource/(?P<id>\w+)/download_attach$', 'download_attach', {}, 'download_attach'),
     (r'^datasource/(?P<id>\w+)/import_data$', 'import_data', {}, 'import_data'),
-    (r'^datasource/(?P<id>\w+)/show_data$', 'show_data', {}, 'show_data'),
+    (r'^datasource/(?P<id>\w+)/show_data', 'show_data', {}, 'show_data'),
 
     (r'^column/(?P<id>\w+)$', 'column_detail', {}, 'column'),
    
